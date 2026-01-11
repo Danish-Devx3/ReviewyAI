05af09fec75fb16787f7088ef202b9e218f9445b
Danish-Devx3 <danish.devx3@gmail.com>
2026-01-11 23:28:41 +0530

    working on frontend

---
+++ app/dashboard/subscription/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+const PLAN_TYPES = {
+    free: [
+        { name: "Up to 5 repositories", included: true },
+        { name: "Up to 5 reviews per repository", included: true },
+        { name: "Basic code review", included: true },
+        { name: "Community support", included: true },
+        { name: "Advanced analytics", included: false },
+        { name: "Priority support", included: false },
+    ],
+
+    pro: [
+        { name: "Up to 5 repositories", included: true },
+        { name: "Up to 5 reviews per repository", included: true },
+        { name: "Basic code review", included: true },
+        { name: "Community support", included: true },
+        { name: "Advanced analytics", included: true },
+        { name: "Priority support", included: true },
+    ],
+}
